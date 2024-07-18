765b7908c3ff25835436c68f9898d06cf631a402
rlimazzz <ryangabryel2005@gmail.com>
2024-07-18 17:28:53 -0300

    oi

--- followingTutos/main.cpp
+++ followingTutos/main.cpp
@@ -4,6 +4,7 @@
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <math.h>
+#include<unistd.h>
 #include <cstdlib>
 
 class Bullet 
@@ -28,13 +29,14 @@ class Player
     public :
         sf::Sprite shape;
         sf::Texture *texture;
+        sf::Sound soundDying;
         int HP;
         int HPMax;
         int score;
 
         std::vector<Bullet> bullets;
 
-        Player(sf::Texture *texture) 
+        Player(sf::Texture *texture, sf::SoundBuffer *dyingBuffer) 
         {
             this->HPMax = 10;
             this->HP = this->HPMax;
@@ -42,6 +44,14 @@ class Player
 
             this->texture = texture;
             this->shape.setTexture(*texture);
+            this->soundDying.setBuffer(*dyingBuffer);
+        }
+
+        void reset()
+        {
+            this->HPMax = 10;
+            this->HP = this->HPMax;
+            this->score = 0;
         }
 
         ~Player() {}
@@ -71,58 +81,133 @@ class Enemy
 
 int main()
 {
+    //Window and some other useful stuff
     sf::RenderWindow window(sf::VideoMode(1366, 720), "Savio Shooters");
     int moveVariation = 10, shootTimer = 20, enemySpawnTimer = 0;
+    sf::Color outLineDecoration(99, 35, 120);
 
     srand(time(NULL));
     window.setFramerateLimit(60);
 
-    //Setting up textures, fonts, sounds, etc
+    //Setting up textures, fonts, sounds, etc, handling possible errors
     sf::Font font;
-    font.loadFromFile("./fonts/airstrike.ttf");
+    if(!font.loadFromFile("./fonts/airstrike.ttf"))
+    {
+        std::cout << "Failed to load airstrike font\n";
+    }
 
     sf::Texture spaceshipTexture;
-    spaceshipTexture.loadFromFile("./sprites/space.png");
+    if(!spaceshipTexture.loadFromFile("./sprites/space.png"))
+    {
+        std::cout<< "Failed to load spaceship Texture\n";
+    }
 
     sf::Texture bulletTexture;
-    bulletTexture.loadFromFile("./sprites/bullet.png");
+    if(!bulletTexture.loadFromFile("./sprites/bullet.png"))
+    {
+        std::cout << "Failed to load bullet texture\n";
+    }
 
     sf::Texture enemyTexture;
-    enemyTexture.loadFromFile("./sprites/cat.png");
+    if(!enemyTexture.loadFromFile("./sprites/cat.png")) 
+    {
+        std::cout << "Failed to load the cat texture\n";
+    }
 
     sf::Texture backgroundTexture;
-    backgroundTexture.loadFromFile("./images/space.jpg");
+    if(!backgroundTexture.loadFromFile("./images/space.jpg"))
+    {
+        std::cout << "Failed to load the space texture\n";
+    }
+
+
+    //Audio things
+    sf::Music bgMusic;
+    if(!bgMusic.openFromFile("./audio/theme.ogg"))
+    {
+        std::cout << "Failed to load the theme song\n";
+    }
+    bgMusic.setVolume(20.f);
+    bgMusic.play();
+    bgMusic.setLoop(true);
+
+    sf::SoundBuffer boomBuffer;
+    if(!boomBuffer.loadFromFile("./audio/boom12.wav"))
+    {
+        std::cout << "Failed to load the boom sound effect\n";
+    }
+
+    sf::SoundBuffer coinBuffer;
+    if(!coinBuffer.loadFromFile("./audio/coin.wav")) 
+    {
+        std::cout << "Failed to load the coin sound effect\n";
+    }
+    sf::Sound coinSound;
+    coinSound.setBuffer(coinBuffer);
+
+    sf::SoundBuffer failedBuffer;
+    if(!failedBuffer.loadFromFile("./audio/lose.wav"))
+    {
+        std::cout << "failed to load the failed sound effect\n";
+    }
+    sf::Sound failedSound;
+    failedSound.setBuffer(failedBuffer);
+
+    sf::SoundBuffer gunfireBuffer;
+    if(!gunfireBuffer.loadFromFile("./audio/gunfire.wav")) 
+    {
+        std::cout << "failed to load the gunfire sound effect\n";
+    }
+    sf::Sound gunfireSound;
+    gunfireSound.setVolume(20.f);
+    gunfireSound.setBuffer(gunfireBuffer);
 
     //Texts
     sf::Text hpText;
     hpText.setFont(font);
+    hpText.setOutlineThickness(5);
+    hpText.setOutlineColor(outLineDecoration);
     hpText.setCharacterSize(20);
     hpText.setFillColor(sf::Color::White);
 
     sf::Text scoreText;
     scoreText.setFont(font);
+    scoreText.setOutlineThickness(5);
+    scoreText.setOutlineColor(outLineDecoration);
     scoreText.setCharacterSize(40);
     scoreText.setFillColor(sf::Color::White);
 
     sf::Text gameOverText;
     gameOverText.setFont(font);
+    gameOverText.setOutlineThickness(5);
+    gameOverText.setOutlineColor(outLineDecoration);
     gameOverText.setCharacterSize(40);
-    gameOverText.setFillColor(sf::Color::Red);
+    gameOverText.setFillColor(sf::Color::White);
 
     sf::Text restartText;
     restartText.setFont(font);
+    restartText.setOutlineThickness(5);
+    restartText.setOutlineColor(outLineDecoration);
     restartText.setCharacterSize(40);
-    restartText.setFillColor(sf::Color::Red);
+    restartText.setFillColor(sf::Color::White);
 
     //Main player initializer
-    Player mPlayer(&spaceshipTexture);
-    mPlayer.shape.setPosition(300.f, 300.f);
+    Player mPlayer(&spaceshipTexture, &boomBuffer);
+    mPlayer.shape.setPosition((window.getSize().x - mPlayer.shape.getGlobalBounds().width)/2, (window.getSize().y - mPlayer.shape.getGlobalBounds().height));
+
+    //Setting up the option for restart and surrender
+    sf::Vector2f sizer(150.f, 50.f);
+    sf::Vector2f unsizer(0.f, 0.f);
+    sf::RectangleShape restartRect;
+    sf::RectangleShape surrenderRect;
 
     //Enemies initializer
     std::vector<Enemy> enemies;
 
+
     while (window.isOpen())
     {
+        //handling events l
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -130,14 +215,57 @@ int main()
             {
                 window.close();
             }
+            if (event.type == sf::Event::Resized) 
+            {
+                sf::FloatRect visibleArea(0.f, 0.f, event.size.width, event.size.height);
+                window.setView(sf::View(visibleArea));
+            }
         }
 
         if(mPlayer.HP <= 0)
         {
-            gameOverText.setPosition(window.getSize().x / 3, window.getSize().y / 3);
+
+            /*
+            This part of the code is very unorganized but basically we are showing the menu of gameOver
+            to our player
+            */
             restartText.setPosition(window.getSize().x / 3, window.getSize().y / 3 + 50);
             restartText.setString("RESTART THE GAME?");
+            restartText.setCharacterSize(40);
+            
+            gameOverText.setPosition(window.getSize().x / 3, window.getSize().y / 3);
             gameOverText.setString("GAME OVER");
+            gameOverText.setCharacterSize(40);
+
+            surrenderRect.setPosition(window.getSize().x / 3 + 250 , window.getSize().y / 3 + 100);
+            surrenderRect.setFillColor(sf::Color::Red);
+            surrenderRect.setSize(sizer);
+
+            restartRect.setPosition(window.getSize().x / 3  , window.getSize().y / 3 + 100);
+            restartRect.setSize(sizer);
+            restartRect.setFillColor(sf::Color::Green);
+
+
+            if(sf::Mouse::isButtonPressed(sf::Mouse::Left)) 
+            {
+                if(restartRect.getGlobalBounds().contains(sf::Mouse::getPosition().x, sf::Mouse::getPosition().y - 50))
+                {
+                    coinSound.play();
+                    mPlayer.reset();
+                    mPlayer.shape.setPosition((window.getSize().x - mPlayer.shape.getGlobalBounds().width)/2, (window.getSize().y - mPlayer.shape.getGlobalBounds().height));
+                    restartRect.setSize(unsizer);
+                    surrenderRect.setSize(unsizer);
+                    restartText.setCharacterSize(0);
+                    gameOverText.setCharacterSize(0);
+                    continue;
+                }
+                else if(surrenderRect.getGlobalBounds().contains(sf::Mouse::getPosition().x, sf::Mouse::getPosition().y - 50))
+                {
+                    failedSound.play();
+                    sleep(3);
+                    window.close();
+                }
+            }
         }else {
             //Timers 
             if(shootTimer < 20) 
@@ -157,6 +285,7 @@ int main()
             //Player Controls
             if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && shootTimer >= 20)
             {
+                gunfireSound.play();
                 mPlayer.bullets.push_back(Bullet(&bulletTexture, mPlayer.shape.getPosition()));
                 shootTimer = 0;
             }
@@ -184,9 +313,6 @@ int main()
             //Score text configuration
             scoreText.setPosition(0, window.getSize().y - 70);
             scoreText.setString("SCORE : " + std::to_string(mPlayer.score));
-
-            //Game over message
-
 
             //Colision with window
             if(mPlayer.shape.getPosition().x <= 0) 
@@ -249,6 +375,7 @@ int main()
                     }
                 }
             }
+
             //Shooting bullets animation
             for(size_t i = 0;i < mPlayer.bullets.size();i++) 
             {
@@ -269,9 +396,16 @@ int main()
                 {
                     enemies.erase(enemies.begin() + i);
                     mPlayer.HP--;
+
+                    if(mPlayer.HP <= 0) 
+                    {
+                        mPlayer.soundDying.play();
+                    }
+
                     break;
                 }
             }
+
             //Spawning enemies
             for(size_t i = 0;i < enemies.size();i++) 
             {
@@ -284,6 +418,8 @@ int main()
         window.draw(restartText);
         window.draw(scoreText);
         window.draw(gameOverText);
+        window.draw(restartRect);
+        window.draw(surrenderRect);
 
         window.display();
     }
